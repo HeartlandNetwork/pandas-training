f45c727fe8a454bc5e6ebed1f16997893be1c82b
HeartlandNetwork <gareth_rowell@nps.gov>
2024-12-13 14:36:02 -0600

    examples downloaded

--- src/Ch6-Data-loading-storage-file-formats.py
+++ src/Ch6-Data-loading-storage-file-formats.py
@@ -3,14 +3,45 @@
 # Ch 6 Data loading, storage and file formats
 # ----------------------------------------------------
 
+# Categories of input and output
+# reading text files and other more efficient on-disk formats
+# load data from databases
+# interacting with network sources like web APIs
+
+
 
 # ----------------------------------------------------
 # 6.1 Reading and writing data in text format
 # ----------------------------------------------------
 
+# pandas.read_csv most frequently used method
+
+# Text and binary data loading functions in pandas
+# read_csv, read_fwf, read_clipboard, read_excel
+# read_hdf, read_html, read_json, read_feather,
+# read_orc, read_parquet, read_pickle, read_sas,
+# read_spss, read_sql, read_sql_table, read_stata,
+# read_xml
+
+# option arguments - general categories as follows
+# Indexing, Type inference and data conversion
+# Date and time parsing, Iterating, 
+# Unclear data issues
+
+# pandas.read_csv has about 50 optional arguments!!
+# be sure to checkout pandas documentation
+
+
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
 
 # Reading text files in pieces
 # ----------------------------
+
+
+
 
 
 
