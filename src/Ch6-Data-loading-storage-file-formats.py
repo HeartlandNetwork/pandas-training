71eff2b8712592d1884d772ec317dec487b80af8
HeartlandNetwork <gareth_rowell@nps.gov>
2024-12-18 11:15:05 -0600

    read_csv and NAs

--- src/Ch6-Data-loading-storage-file-formats.py
+++ src/Ch6-Data-loading-storage-file-formats.py
@@ -52,13 +52,87 @@ pd.read_csv("examples/ex2.csv", header=None)
 pd.read_csv("examples/ex2.csv", names=["a", "b", "c", "d", "message"])
 
 
-
-# Making the mesage column the indexed column
+# Making the message column the indexed column
 
 
 names = ["a", "b", "c", "d", "message"]
 
 names
+
+pd.read_csv("examples/ex2.csv", names=names, index_col="message")
+
+# Forming hierarchical index from multiple columns
+
+parsed = pd.read_csv("examples/csv_mindex.csv", index_col=["key1", "key2"])
+
+parsed
+
+# examples/ex3.txt - a table with variable whitespace. 
+# No fixed delimiter - used "\s+"
+
+result = pd.read_csv("examples/ex3.txt")
+
+result
+
+# Spacial case - one fewer column names than number of data rows
+# read_csv infers first column as index
+
+# Other arguments - e.g., skipping lines
+
+all_lines = pd.read_csv("examples/ex4.csv")
+
+all_lines
+
+skipping_lines = pd.read_csv("examples/ex4.csv", skiprows=[0, 2, 3])
+
+skipping_lines
+
+# Missing values or with placeholders "sentinel" values
+# example sentinels are NA and NULL
+
+result = pd.read_csv("examples/ex5.csv")
+
+result
+
+# masking the NA condition, gives True for the NA
+
+pd.isna(result)
+
+# Using the na_values argument to include strings
+# besides the default recognized by read_csv
+
+result = pd.read_csv("examples/ex5.csv", na_values=["NULL"])
+
+result
+
+# disabling the default NA
+
+result2 = pd.read_csv("examples/ex5.csv", keep_default_na=False)
+
+result2
+
+# then masking the NA condition gives all falses
+
+
+pd.isna(result)
+
+pd.isna(result2)
+
+# Specifying to use only the NAs
+
+result3 = pd.read_csv("examples/ex5.csv", keep_default_na=False,
+                        na_values=["NA"])
+ 
+result3
+
+# Masking for na's gives a true
+
+pd.isna(result3)
+
+# Different NA sentinels can be spedified for each column in a dictionary"
+
+
+
 
 
 
